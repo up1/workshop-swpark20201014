4bd8c75c022f4c42e1bd55605aa451d87a12b72d
up1 <somkiat.p@gmail.com>
2020-10-14 16:50:30 +0700

    Update filename

--- csv/Employee.py
+++ csv/Employee.py
@@ -13,7 +13,7 @@ class Employee:
         return results
 
 if __name__ == '__main__':
-    e = Employee()
+    e = Employee('employees.csv')
     datas = e.read_all()
     for row in datas:
         print(row[0], row[1])
