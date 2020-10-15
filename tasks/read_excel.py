7d87a345a71349db51050b981165dd172b902a98
up1 <somkiat.p@gmail.com>
2020-10-15 14:19:15 +0700

    Update excel

--- tasks/read_excel.py
+++ tasks/read_excel.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     data1 = read_excel_worksheet('sample.xlsx', 'Sheet1')
     for d in data1:    
         print(d['A'], d['B'])
+    
     data2 = read_excel_worksheet('sample.xlsx', 'Sheet2')
     for d in data2:    
         print(d['A'], d['B'], d['C'])
--- tasks/read_excel2.py
+++ tasks/read_excel2.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Read File
 sample1 = pd.read_excel("sample.xlsx", sheet_name="Sheet1")
-print(sample1)
+print(sample1[:3])
 
 sample2 = pd.read_excel("sample.xlsx", sheet_name="Sheet2")
 print(sample2)
